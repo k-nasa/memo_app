9c07a3161e98f86f2cc4f7e6165238c663967837
k-nasa <htilcs1115@gmail.com>
2018-05-18 21:20:20 +0900

    add/ HomaPageWidget and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,109 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:memo_app/databae_helper.dart';
+import 'package:memo_app/new_memo_form.dart';
+import 'package:memo_app/memo.dart';
 
-void main() => runApp(new MyApp());
+var dbHelper = new DBHelper();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'hoge',
+      routes: <String, WidgetBuilder> {
+        '/': (_) => new Home(),
+        '/new_memo': (_) => new MemoForm()
+      },
+    )
+  );
+}
+
+class Home extends StatelessWidget {
+  var _db = new DBHelper();
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('HOME'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.add_circle),
+            onPressed: (){
+              Navigator.of(context).pushNamed('/new_memo');
+            },
+          )
+        ],
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      body: new MemoScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MemoScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State createState() => new MemoScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+Future<List<MemoMessage>> getMemoListWidget() async {
+  dbHelper.getMemos().then((List<Memo> list) {
+    if(list == [])  return [];
+    List<MemoMessage> memoList = [];
+    list.map((memo) {
+      memoList.add(
+          new MemoMessage(
+              content: memo.content,
+              finished: memo.finished
+          ));
     });
+    return memoList;
+  });
+}
+// 仮実装
+class MemoScreenState extends State<MemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new FutureBuilder(
+              future: getMemoListWidget(),
+              builder: (context, snapshot) {
+                if(snapshot.hasData) {
+                  return new ListView.builder(
+                    padding: new EdgeInsets.all(8.0),
+                    reverse: true,
+                    itemBuilder: (_, int index) => snapshot.data[index],
+                    itemCount: snapshot.data.length,
+                  );
+                }else {
+                  return new Container();
+                }
+              },
+            ),
+          ),
+          new Divider(height: 2.0,) ,
+        ],
+      ),
+    );
   }
+}
+
+// 仮実装
+class MemoMessage extends StatelessWidget {
+  MemoMessage({this.content, this.finished});
+
+  String content;
+  bool finished;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return new Row(
+      children: <Widget>[
+        new Text(content),
+        new Text(finished.toString()),
+      ],
     );
   }
 }
